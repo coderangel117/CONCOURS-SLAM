dfd840b9e5ff3e347ac35f13e8ab49a366e0a8a3
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 22:49:29 +0200

    [addUser] : create the file and just add first input

---
+++ admin/addUser.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../elements/header.php';
+?>
+    <form action="">
+        <label for="nom">nom</label>
+        <input type="text" placeholder="Veuillez entrer le nom de famille">
+    </form>
+<?php
+require_once __DIR__ . '/../elements/footer.php';
