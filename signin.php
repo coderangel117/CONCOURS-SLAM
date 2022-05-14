64e92e56843956dcc5de4eeca6bd717ae7695783
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 23:07:04 +0200

    [signin] : create the file and 2C the content from login.php

---
+++ signin.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . '/elements/header.php';
+?>
+    <form method="POST">
+        <label for="login">Entrez votre identifiant</label>
+        <input id="login" type="text"><br>
+        <label for="password">Entrez votre mot de passe</label>
+        <input id="password" type="password"><br>
+        <button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
+
+        Pas encore de compte? <a href="signin.php" target="_blank">créez-en un ici </a>
+    </form
+<?php
+require_once __DIR__ . '/elements/footer.php';
