ac4c5624e464d7382e7e76d5cd5fc9c9bf832dee
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 22:52:43 +0200

    [admin/index] : create the file and add link

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../elements/header.php';
+?>
+    <h1> Bienvenue dans l'appli de gestion des scout</h1>
+    <h2>vous devez vous connecter</h2>
+    <a href="../listScout.php" target="_blank">scout</a>
+    <br><a href="../listGuide.php" target="_blank"> guides</a>
+<?php
+require_once __DIR__ . '/../elements/footer.php';
