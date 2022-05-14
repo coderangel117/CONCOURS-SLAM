6611bc47875affe12094133df4f2197bb6d72f29
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 22:53:17 +0200

    [index] :  add links to others pages

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 require_once __DIR__ . '/elements/header.php';
 ?>
     <h1> Bienvenue dans l'appli de gestion des scout</h1>
+<h2>vous devez vous <a href="login.php" target="_blank">connecter</a></h2>
+    <a href="listScout.php" target="_blank">scout</a>
+    <br><a href="listGuide.php" target="_blank"> guides</a>
+    <a href="admin/addUser.php"> seulement administrateur</a>
 <?php
 require_once __DIR__ . '/elements/footer.php';
