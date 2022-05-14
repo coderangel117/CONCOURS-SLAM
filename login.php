9d04c53fcd9a6f01b0f47a88de1b45c05b628569
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 23:01:46 +0200

    [login] : change type password input text ==> password  / add button to confirm

--- login.php
+++ login.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/elements/header.php';
         <label for="login">Entrez votre identifiant</label>
         <input id="login"  type="text"><br>
         <label for="password">Entrez votre mot de passe</label>
-        <input id="password"  type="text">
-
+        <input id="password"  type="password"><br>
+<button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
     </form>
 <?php
 require_once __DIR__ . '/elements/footer.php';
