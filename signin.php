c5c40396e8baa652809c6a534b25d484f80016c0
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 23:49:09 +0200

    [signin] : replace the button after the checkbox / add script code to enable the button only when the checkbox is checked

--- signin.php
+++ signin.php
@@ -8,14 +8,23 @@ require_once __DIR__ . '/elements/header.php';
         <input id="password" type="password"><br>
         <label for="confirmPassword">Confirmer votre mot de passe</label>
         <input id="confirmPassword" type="password"><br>
-        <button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
         <br>
         <label> Veuillez cocher la case pour continuer
-            <input type="checkbox" name="rgpd">
+            <input type="checkbox" name="rgpd" id="rgpd" onchange="Accept_RGPD()">
         </label><br>
         <p>     En créant votre compte vous acceptez le traitement de vos données.
         Pour rappel vous avez le droit d'accès de consultation, de modification et de suppression à tout moment </p>
         <br>
-    </form
+        <button type="submit" name="btnconfirmlogin" id="btnconfirmlogin" disabled > Valider</button> <!--le bouton est désactivé tant que la case au dessus n'est pas cochée-->
+    </form>
+    <script>
+        let check_RGPD = document.getElementById('rgpd');
+        let validate = document.getElementById('btnconfirmlogin');
+        function Accept_RGPD() {
+            if (check_RGPD) {
+                validate.removeAttribute('disabled')
+            }
+        }
+    </script>
 <?php
-require_once __DIR__ . '/elements/footer.php';
+require_once __DIR__ . '/elements/footer.php';?>
