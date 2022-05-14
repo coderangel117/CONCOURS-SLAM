caeaeb0fe04640b62fdf815c6d840f1afde5915c
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 22:54:30 +0200

    [login] : create the file and just add first input

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/elements/header.php';
+?>
+    <form action="">
+        <label for="login">Entrez votre identifiant</label>
+        <input id="login"  type="text"><br>
+        <label for="password">Entrez votre mot de passe</label>
+        <input id="password"  type="text">
+
+    </form>
+<?php
+require_once __DIR__ . '/elements/footer.php';
