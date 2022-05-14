426dc3d02bbae1e8068e8b7e7513097c619abcc7
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 23:30:09 +0200

    [signin] : add confirmPassword / add checkbox for GDPR

--- signin.php
+++ signin.php
@@ -6,9 +6,16 @@ require_once __DIR__ . '/elements/header.php';
         <input id="login" type="text"><br>
         <label for="password">Entrez votre mot de passe</label>
         <input id="password" type="password"><br>
+        <label for="confirmPassword">Confirmer votre mot de passe</label>
+        <input id="confirmPassword" type="password"><br>
         <button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
-
-        Pas encore de compte? <a href="signin.php" target="_blank">créez-en un ici </a>
+        <br>
+        <label> Veuillez cocher la case pour continuer
+            <input type="checkbox" name="rgpd">
+        </label><br>
+        <p>     En créant votre compte vous acceptez le traitement de vos données.
+        Pour rappel vous avez le droit d'accès de consultation, de modification et de suppression à tout moment </p>
+        <br>
     </form
 <?php
 require_once __DIR__ . '/elements/footer.php';
