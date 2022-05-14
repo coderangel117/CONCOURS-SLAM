0e2390504c0596f5b02a8e39103fc0ec163e7ca7
Gabriel PERINO <gabriel.perino@lyceefulbert.fr>
2022-05-14 23:06:23 +0200

    [login] : add link to create a new account / reformat code

--- login.php
+++ login.php
@@ -3,10 +3,12 @@ require_once __DIR__ . '/elements/header.php';
 ?>
     <form action="">
         <label for="login">Entrez votre identifiant</label>
-        <input id="login"  type="text"><br>
+        <input id="login" type="text"><br>
         <label for="password">Entrez votre mot de passe</label>
-        <input id="password"  type="password"><br>
-<button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
-    </form>
+        <input id="password" type="password"><br>
+        <button type="submit" name="btnconfirmlogin" id="btnconfirmlogin"> Valider</button>
+
+        Pas encore de compte? <a href="signin.php" target="_blank">créez-en un ici </a>
+    </form
 <?php
 require_once __DIR__ . '/elements/footer.php';
